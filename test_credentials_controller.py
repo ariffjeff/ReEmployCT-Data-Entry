93d20a81b357c0552f7bd60354b2fe6d23e41345
Ariff <ariffjeff@icloud.com>
2022-11-24 19:27:59 -0500

    unit test credential variable names

---
+++ test_credentials_controller.py
@@ -0,0 +1,20 @@
+import unittest
+from controller_credentials import Credentials as Creds
+
+class TestCredCon(unittest.TestCase):
+
+  def test_formattedCredStringIniOutput(self):
+    '''
+    Ensure the number of formatted output keys in the .ini credentials file equals the number of corresponding name-matching attributes from __init__
+    Assertion would fail when any of the key names in userCreds.ini_file_keys() are not written exactly the same as the corresponding attribute names from __init__
+    This is to force name parity between the desired credential key attribs in __init__ and the keys that actually output to the .ini file in order to reduce complexity
+    For example, this unit test fails if the __init__ key name_first is written as first_name in userCreds.ini_file_keys()
+    '''
+    userCreds = Creds()
+    formatted_credential_string = userCreds.create_formatted_cred_output_string()
+    KEYS = userCreds.ini_file_keys()
+    self.assertEqual(formatted_credential_string.rstrip('\n').count('\n') + 1, len(KEYS))
+
+
+if __name__ == '__main__':
+  unittest.main()
