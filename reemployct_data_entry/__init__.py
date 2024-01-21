98c7a35d937c0c0b2b703a24d07015c572b36300
Ariff Jeff <ariffjeff@icloud.com>
2024-01-21 01:12:10 -0500

    2.0.4

--- reemployct_data_entry/__init__.py
+++ reemployct_data_entry/__init__.py
@@ -3,7 +3,7 @@ import colorama
 PACKAGE_NAME = 'reemployct_data_entry'
 
 # can't use stdlib importlib.metadata here because remote tests will fail due to needing the package build
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 # on `import reemployct_data_entry`, print the excel job data template file path for user convenience
 if __name__ == PACKAGE_NAME:
